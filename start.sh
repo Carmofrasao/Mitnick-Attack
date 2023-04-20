694fae5540b0ed6fe05814a130ee99bd8599e281
Gobbedu <gobbolegal@gmail.com>
2023-04-20 00:19:21 -0300

    atacante no docker

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod +x startup.sh
-
 # constroi imagem docker
 docker build --rm -t base:1.0 Containers/ 
 
@@ -22,21 +20,27 @@ else
     docker run --name xterminal -p 10001:222 base:1.0&
 fi
 
+if [ "$(docker ps -aq --filter name=atacante)" ]; then
+    echo "Xterminal container already exists"
+    docker start atacante
+else
+    echo "Xterminal container doesnt exists, creating"
+    docker run --name atacante -p 10001:222 base:1.0&
+fi
+
 
 gnome-terminal \
     --tab --title="xterminal" --command "docker exec -it xterminal /bin/bash" \
+    --tab --title="atacante" --command "docker exec -it atacante /bin/bash" \
     --tab --title="server" --command "docker exec -it server /bin/bash" &> /dev/null
 
-# cansei
+
 id_xterm=$(docker ps --filter name="xterminal" --format "{{.ID}}")
 id_server=$(docker ps --filter name="server" --format "{{.ID}}")
+id_atack=$(docker ps --filter name="atacante" --format "{{.ID}}")
 
 echo "SETAR CONTAINER"
 echo -e "\t\tIP\t\t\tnome\t\tId"
 echo -e "$(docker inspect server | grep -m 1 IPAddress\"\:)\tserver\t\t$id_server"
 echo -e "$(docker inspect xterminal | grep -m 1 IPAddress\"\:)\txterminal\t$id_xterm"
-
-# bash container.sh id_xterm $id_xterm > _xterm.sh
-# bash container.sh id_server $id_server > _server.sh
-
-# cat _xterm.sh _server.sh
+echo -e "$(docker inspect atacante | grep -m 1 IPAddress\"\:)\txterminal\t$id_atack"
